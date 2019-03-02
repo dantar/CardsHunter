48725f278e9131aa7a93fd61bc77ee4b112c635e
Daniele Tarini <d.tarini@hyperborea.com>
2019-03-02 12:34:36 +0100

    Game basic interface and settings allows to fully play the tutorial

--- src/providers/event-manager/event-manager.ts
+++ src/providers/event-manager/event-manager.ts
@@ -19,10 +19,19 @@ export class EventManagerProvider {
       {
         trigger: new HtInitGame(),
         effect: new HcMany([
-          new HcGainItem('shield'),
-          new HcMessage('Welcome to the game!'),
-          new HcMessage('This is another longer message. It is three sentences long. This is the last sentence.'),
-          new HcMessage('Good luck!'),
+          new HcMessage('Ciao! Benvenuto in questo tutorial del gioco.'),
+          new HcMessage('Prendi il mazzo di carte, ma non guardarle. Tienilo a portata di mano.'),
+          new HcMessage('Via via che affronti il gioco ti verrà detto quali carte scoprire e quando. ' +
+            'Per cercare una carta da scoprire scorri il mazzo coperto e trova la carta giusta dalle indicazioni sul dorso. ' +
+            'Scopri solo le carte che ti vengono indicate.'),
+          new HcMessage('Il gioco si svolge attivando le carte. '),
+          new HcMessage('Ad ogni mossa del gioco potrai decidere se attivare una o due carte fra quelle che hai scoperte. '),
+          new HcMessage('Per attivare una carta, clicca "Attiva 1 carta" e inquadra il suo codice.'),
+          new HcMessage('Per attivare una combinazione di due carte, clicca "Attiva 2 carte" e inquadra il loro codice, una carta per volta. ' +
+            'Non è importante in che ordine attivi le due carte della combinazione. '),
+          new HcMessage('In bocca al lupo!'),
+          new HcMessage('Scopri la carta A.'),
+          new HcGainItem('ship'),
         ]),
       },
       {
@@ -47,8 +56,54 @@ export class EventManagerProvider {
         ]),
       },
       {
+        trigger: new HtWithItem('bridge', 'orcs'),
+        effect: new HcMany([
+          new HcDropItem('sword'),
+          new HcMessage('You broke the sword!'),
+        ]),
+      },
+      {
+        trigger: new HtClickItem('ship'),
+        effect: new HcMany([
+          new HcMessage('Esplori la nave pirata abbandonata e trovi una vecchia mappa.'),
+          new HcMessage('Nella cabina del capitano trovi anche un sacchetto di cracker.'),
+          new HcMessage('Scopri le carte B e C'),
+          new HcGainItem('map'),
+          new HcGainItem('cracker'),
+        ]),
+      },
+      {
+        trigger: new HtClickItem('map'),
+        effect: new HcMany([
+          new HcMessage('La mappa indica il luogo preciso dove si trova il tesoro. Vicino ad un albero, poco sotto la sabbia, trovi uno scrigno chiuso a chiave.'),
+          new HcMessage('Scopri le carte D e E'),
+          new HcGainItem('parrot'),
+          new HcGainItem('chest'),
+        ]),
+      },
+      {
+        trigger: new HtWithItem('cracker', 'parrot'),
+        effect: new HcMany([
+          new HcMessage('Il pappagallo prende con il becco il cracker e lascia cadere la chiave, quindi se ne svolazza via contento.'),
+          new HcMessage('Scopri le carta F'),
+          new HcGainItem('key'),
+        ]),
+      },
+      {
+        trigger: new HtWithItem('key', 'chest'),
+        effect: new HcMany([
+          new HcMessage('Con un "clack" la chiave apre il lucchetto dello scrigno!'),
+          new HcMessage('Complimenti, hai trovato il tesoro: hai completato questo tutorial!'),
+          //new HcEndGame('key'),
+        ]),
+      },
+      {
+        trigger: new HtClickItem('parrot'),
+        effect: new HcMessage('Il pappagallo tiene una vecchia chiave ben salda nel becco. Chissà dove l\'ha trovata.'),
+      },
+      {
         trigger: new HtNoMessages(),
-        effect: new HcMessage('Nothing happens'),
+        effect: new HcMessage('Non accade nulla...'),
       },
     ];
   }
